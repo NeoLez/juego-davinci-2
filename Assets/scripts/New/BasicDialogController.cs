70a0b5389b756ad0760a405d91ebca05499b21a6
Leo Gonzalez <66876432+NeoLez@users.noreply.github.com>
2024-11-17 14:41:15 -0300

    d

--- Assets/scripts/New/BasicDialogController.cs
+++ Assets/scripts/New/BasicDialogController.cs
@@ -6,10 +6,30 @@ namespace New
 	public class BasicDialogController : MonoBehaviour
 	{
 		[SerializeField] private Dialogue _dialogue;
+		private bool isPlayerInRange;
 
 		private void Update() {
-			if (_dialogue.IsPlayerInRange() && Input.GetKeyDown(KeyCode.F)) { ;
+			if (isPlayerInRange && Input.GetKeyDown(KeyCode.F)) { ;
 				_dialogue.Interact();
+			}
+		}
+		
+		
+		private void OnTriggerEnter2D(Collider2D Collision)
+		{
+			if (Manager.Instance.player == Collision.gameObject)
+			{
+				isPlayerInRange = true;
+				_dialogue.SetDialogMarkState(true);
+			}
+
+		}
+		private void OnTriggerExit2D(Collider2D Collision)
+		{
+			if (Manager.Instance.player == Collision.gameObject)
+			{
+				isPlayerInRange = false;
+				_dialogue.SetDialogMarkState(false);
 			}
 		}
 	}
--- Assets/scripts/New/Materials/ExitDoorDialogController.cs
+++ Assets/scripts/New/Materials/ExitDoorDialogController.cs
@@ -1,7 +1,57 @@
-﻿namespace New.Materials
+﻿using UnityEngine;
+
+namespace New.Materials
 {
-	public class ExitDoorDialogController
+	public class ExitDoorDialogController : MonoBehaviour
 	{
+		[SerializeField] private Dialogue dialogDoorClosed;
+		[SerializeField] private Dialogue dialogDoorOpen;
+		private bool isPlayerInRange;
 		
+		private bool isDoorOpen;
+		private bool lockState;
+		
+		private void Update() {
+			if (!lockState) {
+				if (Manager.Instance.foundKeyOne && Manager.Instance.foundKeyTwo) {
+					isDoorOpen = true;
+				}
+				else {
+					isDoorOpen = false;
+				}
+			}
+
+			if (isPlayerInRange && Input.GetKeyDown(KeyCode.F)) {
+				lockState = true;
+				if (isDoorOpen) {
+					lockState = dialogDoorOpen.Interact();
+					if (!lockState) {
+						Debug.Log("SceneChange");
+					}
+				}
+				else {
+					lockState = dialogDoorClosed.Interact();
+				}
+			}
+		}
+		
+		
+		private void OnTriggerEnter2D(Collider2D Collision)
+		{
+			if (Manager.Instance.player == Collision.gameObject)
+			{
+				isPlayerInRange = true;
+				dialogDoorOpen.SetDialogMarkState(true);
+			}
+
+		}
+		private void OnTriggerExit2D(Collider2D Collision)
+		{
+			if (Manager.Instance.player == Collision.gameObject)
+			{
+				isPlayerInRange = false;
+				dialogDoorOpen.SetDialogMarkState(false);
+			}
+		}
 	}
 }
