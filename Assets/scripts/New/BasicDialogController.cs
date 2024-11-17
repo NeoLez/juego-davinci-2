ef4b68ba1f2eaa0618e541ee4c5141fc5c9fc8d1
Leo Gonzalez <66876432+NeoLez@users.noreply.github.com>
2024-11-17 14:09:54 -0300

    a

---
+++ Assets/scripts/New/BasicDialogController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace New
+{
+	public class BasicDialogController : MonoBehaviour
+	{
+		[SerializeField] private Dialogue _dialogue;
+
+		private void Update() {
+			if (_dialogue.IsPlayerInRange() && Input.GetKeyDown(KeyCode.F)) { ;
+				_dialogue.Interact();
+			}
+		}
+	}
+}
