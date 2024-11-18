c3be316210c108a349f384030e96c8f51a31f2b8
Leo Gonzalez <66876432+NeoLez@users.noreply.github.com>
2024-11-17 23:18:21 -0300

    IdleWorking

--- Assets/scripts/New/PlayerAnimationStuff.cs
+++ Assets/scripts/New/PlayerAnimationStuff.cs
@@ -18,9 +18,12 @@ public class PlayerAnimationStuff : MonoBehaviour
 
     void Update() {
         Vector2 moveVector = movement.GetDirectionVector();
-        Debug.Log(moveVector);
+        Vector2 lastMoveVector = movement.GetLastMoveVector();
+        
         playerAnimator.SetFloat("Horizontal", moveVector.x);
         playerAnimator.SetFloat("Vertical", moveVector.y);
         playerAnimator.SetFloat("Speed", moveVector.magnitude);
+        playerAnimator.SetFloat("LastHorizontal", lastMoveVector.x);
+        playerAnimator.SetFloat("LastVertical", lastMoveVector.y);
     }
 }
