f28a517a070e98017db935e6346d326b4dafbb81
Leo Gonzalez <66876432+NeoLez@users.noreply.github.com>
2024-11-18 03:44:25 -0300

    bug

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -6,11 +6,7 @@ namespace New
 {
 	public class PlayerMovement : MonoBehaviour
 	{
-		private Movement movement;
-		private void Awake() {
-			movement = GetComponent<Movement>();
-			Assert.IsNotNull(movement,"Movement component missing from player - " + gameObject.name);
-		}
+		[SerializeField] private Movement movement;
 
 		private void Update() {
 			movement.MoveNormalized(new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical")));
