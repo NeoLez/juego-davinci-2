f746e96341dd819289320f2528bb6684f7549e3e
NeoLez <leogonzachi@gmail.com>
2025-03-06 20:42:53 -0300

    Cajas mas precisas

--- Assets/scripts/Box/PressurePlate.cs
+++ Assets/scripts/Box/PressurePlate.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,38 +11,39 @@ public class PressurePlate : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip activateSound;
     public AudioClip deactivateSound;
+    public float minTriggerDistance = 0.1f;
 
     private void Start()
     {
         puzzleManager = FindObjectOfType<PuzzleManager>();
     }
+    
 
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Box"))
         {
+            float distance = (collision.gameObject.transform.position - gameObject.transform.position).magnitude;
+            
             if (!isPressed) 
             {
-                isPressed = true;
-                audioSource.PlayOneShot(activateSound);
-                Debug.Log("Placa activada: " + gameObject.name);
-                puzzleManager.PlateStateChanged(this, true); 
+                if (distance < minTriggerDistance)
+                {
+                    isPressed = true;
+                    audioSource.PlayOneShot(activateSound);
+                    Debug.Log("Placa activada: " + gameObject.name);
+                    puzzleManager.PlateStateChanged(this, true); 
+                }
+            }else {
+                if (!(distance < minTriggerDistance))
+                {
+                    isPressed = false;
+                    audioSource.PlayOneShot(deactivateSound);
+                    Debug.Log("Placa desactivada: " + gameObject.name);
+                    puzzleManager.PlateStateChanged(this, false); 
+                }
             }
         }
     }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Box"))
-        {
-            if (isPressed) 
-            {
-                isPressed = false;
-                audioSource.PlayOneShot(deactivateSound);
-                Debug.Log("Placa desactivada: " + gameObject.name);
-                puzzleManager.PlateStateChanged(this, false); 
-            }
-        }
-    }
+    
 }
